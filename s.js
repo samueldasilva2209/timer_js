7339b1005107de3aef34b68e84b03376294a805b
samuel santos <samueldasilva2209@gmail.com>
2021-12-01 23:33:30 -0300

    primeiro commit

---
+++ s.js
@@ -0,0 +1,39 @@
+let n = 0 ;
+let aumenta = () => {
+    n+=60 ;
+    document.getElementById('number').innerHTML = n + 'sec';
+} 
+let diminui = () => { 
+    n-=60 ;
+    document.getElementById('number').innerHTML = n + 'sec';
+}
+
+function sub () { 
+    
+    n--
+
+
+    document.getElementById('number').innerHTML = n + 'sec';
+}
+function comeca(){ 
+ 
+
+    let a = setInterval(sub,1000) ;
+
+    let bot = document.querySelector('#comeca'); 
+    bot.disabled = true ; // desativando o botão
+
+}
+
+function parar() {
+    location.reload()
+}
+
+
+
+
+
+
+
+
+
