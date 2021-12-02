a9aadcdca85ce39d6e1b56fefb32181d56b001d9
samuel santos <samueldasilva2209@gmail.com>
2021-12-01 23:42:20 -0300

    mais algumas coisas

--- timer.js
+++ timer.js
@@ -1,39 +1,25 @@
-let n = 0 ;
+let n = 0;
 let aumenta = () => {
-    n+=60 ;
-    document.getElementById('number').innerHTML = n + 'sec';
-} 
-let diminui = () => { 
-    n-=60 ;
-    document.getElementById('number').innerHTML = n + 'sec';
+  n += 60;
+  document.getElementById("number").innerHTML = n + "sec";
+};
+let diminui = () => {
+  n -= 60;
+  document.getElementById("number").innerHTML = n + "sec";
+};
+
+function sub() {
+  n--;
+
+  document.getElementById("number").innerHTML = n + "sec";
 }
+function comeca() {
+  let a = setInterval(sub, 1000);
 
-function sub () { 
-    
-    n--
-
-
-    document.getElementById('number').innerHTML = n + 'sec';
-}
-function comeca(){ 
- 
-
-    let a = setInterval(sub,1000) ;
-
-    let bot = document.querySelector('#comeca'); 
-    bot.disabled = true ; // desativando o botão
-
+  let bot = document.querySelector("#comeca");
+  bot.disabled = true; // desativando o botão
 }
 
 function parar() {
-    location.reload()
+  location.reload();
 }
-
-
-
-
-
-
-
-
-
