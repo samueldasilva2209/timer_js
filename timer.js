a3ab939f13ceaf48213fe401a80b179371012570
samuel santos <samueldasilva2209@gmail.com>
2021-12-05 17:51:02 -0300

    adição de segindos e milésimos

--- timer.js
+++ timer.js
@@ -1,17 +1,30 @@
+
+
+
 let n = 0;
+
+
 let aumenta = () => {
   n += 60;
-  document.getElementById("number").innerHTML = n + "sec";
+  document.getElementById("number").innerHTML = Math.round(n/60) + " min";
+  document.querySelector('.minutos').innerHTML =n + ' sec';
+  document.querySelector('.milesimos').innerHTML = n * 1000+'<br>milésimos'  ;
+  
 };
 let diminui = () => {
   n -= 60;
-  document.getElementById("number").innerHTML = n + "sec";
+  document.getElementById("number").innerHTML = Math.round(n/60) + " min";
+  document.querySelector('.minutos').innerHTML =n + ' sec';
+  document.querySelector('.milesimos').innerHTML = n * 1000+'<br>milésimos'  ;
+
 };
 
 function sub() {
   n--;
 
-  document.getElementById("number").innerHTML = n + "sec";
+  document.getElementById("number").innerHTML = Math.round(n/60) + " min";
+  document.querySelector('.minutos').innerHTML =n + ' sec';
+  document.querySelector('.milesimos').innerHTML = n * 1000 +'<br>milésimos' ;
 }
 function comeca() {
   let a = setInterval(sub, 1000);
@@ -23,3 +36,8 @@ function comeca() {
 function parar() {
   location.reload();
 }
+
+
+  
+
+ 
